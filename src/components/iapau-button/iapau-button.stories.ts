252f1acaf72931b8be90323c7340a433ae6a4b94
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-05-19 21:41:34 +0200

    select button size

--- src/components/iapau-button/iapau-button.stories.ts
+++ src/components/iapau-button/iapau-button.stories.ts
@@ -1,28 +1,35 @@
-import { html, TemplateResult } from "lit";
-import "./iapau-button.js";
+import { Size } from './iapau-button.js';
 
-import { makeStory } from "../../stories/lib.js";
+import { makeStory } from '../../stories/lib.js';
 
 export default {
-  title: "<iapau-button>",
-  component: "iapau-button",
+  title: '<iapau-button>',
+  component: 'iapau-button',
   argTypes: {
-    borderSolid: { control: "boolean" },
+    borderSolid: {
+      control: 'boolean',
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
     size: {
-      control: "text",
-      table: { defaultValue: { summary: "md" } },
-      description: "Button size",
+      mapping: Object.values(Size),
+      options: Object.values(Size),
+      control: {
+        type: 'select',
+      },
+      table: {
+        defaultValue: { summary: 'md' },
+      },
     },
   },
 };
 
-const baseItems = [
-  { innerHTML: "Click here" },
-];
+const baseItems = [{ innerHTML: 'Click here' }];
 
 const conf = {
-  component: "iapau-button",
-  displayMode: "flex-wrap",
+  component: 'iapau-button',
+  displayMode: 'flex-wrap',
   css: `
     :host {
       align-items: center;
@@ -30,16 +37,11 @@ const conf = {
   `,
 };
 
-export const button = makeStory(conf, {
-  items: baseItems
+export const Default = makeStory(conf, {
+  items: baseItems,
 });
 
-
 export const size = makeStory(conf, {
-  docs:
-    `Use the \`size\` parameter to switch button size`,
-  items: [
-    { innerHTML: "Small", size: "sm" },
-    { innerHTML: "Medium" },
-  ],
+  docs: `Use the \`size\` parameter to switch button size`,
+  items: [{ innerHTML: 'Small', size: 'sm' }, { innerHTML: 'Medium' }],
 });
