d91710e6ebfb5370123a79dea19bfeb991c20362
axelvuillaume <axelvuillaume@hotmail.fr>
2023-05-30 16:50:09 +0200

    add text area

---
+++ src/components/ iapau-textarea/iapau-textarea.stories.ts
@@ -0,0 +1,35 @@
+import "./iapau-textarea.js";
+
+import { makeStory } from "../../stories/lib.js";
+
+export default {
+  title: "<iapau-textarea>",
+  component: "iapau-textarea",
+  argTypes: {
+    resize: {
+      control: "boolean",
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
+    placeholder: { control: "text" },
+    cols: { control: "number" },
+    rows: { control: "number" },
+  },
+};
+
+const baseItems = [{ placeholder: "Test clique", cols: 40, rows: 1 }];
+
+const conf = {
+  component: "iapau-textarea",
+  displayMode: "flex-wrap",
+  css: `
+    :host {
+      align-items: center;
+    }
+  `,
+};
+
+export const Default = makeStory(conf, {
+  items: baseItems.map((e) => ({...e , resize:false})),
+});
---
+++ src/components/ iapau-textarea/iapau-textarea.ts
@@ -0,0 +1,51 @@
+import { css, html, LitElement } from 'lit';
+import { property } from 'lit/decorators.js';
+import { classMap } from "lit/directives/class-map.js";
+
+
+export default class IapauTextarea extends LitElement {
+  static styles = css`
+    textarea {
+      border: none;
+      box-shadow: 0px 10px 40px 0px rgba(0,0,0,0.32);
+      border-radius: 15px;
+      padding: 14px;
+      overflow: hidden;
+      font-family: var(--iapau-font-family-default);
+      resize: none;
+    }
+
+    .textarea_resizable{
+      resize: both;
+    }
+  `;
+
+  @property({ type: Number })
+  rows = 1;
+
+  @property({ type: Number })
+  cols = 10;
+
+  @property({ type: String })
+  placeholder = 'Cliquez ici';
+
+  @property({ type: Boolean })
+  resize = false;
+
+  render() {
+    const classes = {
+      textarea_resizable :this.resize,
+    };
+
+    return html`
+      <textarea
+                placeholder='${this.placeholder}'
+                rows="${this.rows}"
+                cols="${this.cols}"
+                class=${classMap(classes)}
+      ></textarea>
+    `;
+  }
+}
+
+window.customElements.define('iapau-textarea', IapauTextarea);
